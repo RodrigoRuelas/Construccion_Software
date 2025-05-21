8b30e23619f24399c26ff0b7b2bdeb2d7321b980
Rodrigo <103969147+RodrigoRuelas@users.noreply.github.com>
2025-05-20 19:28:08 -0500

    Inicios Vue

---
+++ Vue/simple_app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <MethodsComponent/>
+    <HelloWorld/>
+    <simpleComponent/>
+    <dataComponent msg="Mensaje desde App.vue"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import simpleComponent from './components/simpleComponent'
+import dataComponent from './components/dataComponent'
+import MethodsComponent from './components/MethodsComponent'
+
+export default {
+  name: 'App',
+  components: {
+    MethodsComponent,
+    HelloWorld,
+    simpleComponent,
+    dataComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/simple_app/src/components/simpleComponent.vue
@@ -0,0 +1,22 @@
+<template>
+<div class='simple_component'>
+    <h2>This is a component</h2> 
+    my_var: {{ my_var }}     
+</div>
+</template>
+
+<script>
+export default{    
+    data(){ 
+        return {
+            my_var: 'Hello'            
+        }
+    }
+}
+</script>
+
+<style>
+    .simple_component{
+        background-color: #eeeeee;
+    }
+</style>
